60689b8ae4d92baad23c3bb72d1016668bd1973e
Brendon McCool <BMccool@logikos.com>
2023-12-07 14:40:27 -0500

    Fix 2023.07

--- 2023/Day07/test_07.py
+++ 2023/Day07/test_07.py
@@ -43,20 +43,29 @@ class Hand:
         return f"{self.__str__()}"
     
     def __post_init__(self) -> None:
-        self.primary_score = self.get_primary_score()
-        self.secondary_score = self.get_secondary_score()
-        logger.debug(f"Hand: {self.__str__()} Type: {self.type}, Score: {self.score}, Primary Score: {self.primary_score}, Secondary Score: {self.secondary_score}")
+        self.primary_score = self.get_primary_score_number()
+        self.secondary_score = self.get_secondary_score_number()
+        self.score = int(hex(self.primary_score) + hex(self.secondary_score).strip("0x"), 16)
+        logger.info(f"Hand: {self.__str__()} Type: {self.type}, Score: {self.score}, Primary Score: {self.primary_score}, Secondary Score: {self.secondary_score}")
 
     def __lt__(self, other: Self) -> bool:
+        logger.info(f"Comparing {self} and {other}")
         if self.primary_score < other.primary_score:
             return True
         logger.debug(f"Tiebreak check for {self} and {other}")
         for card_i in range(5):
+            logger.info(f"Is {self.cards[card_i]} less than {other.cards[card_i]}?")
             if self.cards[card_i].value < other.cards[card_i].value:
+                logger.info("YES")
+                logger.info(f"Card {self.cards[card_i]} is less than {other.cards[card_i]}")
                 return True
+            elif self.cards[card_i].value > other.cards[card_i].value:
+                logger.info("NO")
+                return False
         return False
     
     def __le__(self, other: Self) -> bool:
+        logger.info(f"Comparing {self} and {other}")
         if self.primary_score < other.primary_score:
             return True
         logger.debug(f"Tiebreak check for {self} and {other}")
@@ -65,28 +74,24 @@ class Hand:
                 return True
         return False
 
-    def get_secondary_score(self) -> int:
+    def get_secondary_score_number(self) -> int:
         """ Get the tiebreaker score"""
         # Must be < 1000, the resolution of the primary score
         # Score each card and give weight to first cards
-        secondary_score = 0
-        for i, card in enumerate(self.cards):
-            weight = ((len(self.cards) - i)) ** 4
-            secondary_score += (card.value * weight)
-            logger.debug(f"Card: {card.label}, Value: {card.value}, Weight: {weight}, Weighted Value {card.value * weight}")
-        logger.debug(f"Secondary Score: {secondary_score}")
-        self.score += secondary_score
-        return secondary_score
+        secondary_score = int("0x" + "".join([hex(card.value).strip("0x") for card in self.cards]), 16)
+        logger.info(f"Secondary Score: {secondary_score}")
+        self.secondary_score = secondary_score
+        return self.secondary_score
     
-    def get_primary_score(self) -> int:
+    def get_primary_score_number(self) -> int:
         """ Score the hand """
-        TYPES = {"5oaK": 600000,
-                 "4oaK": 500000, 
-                 "FH": 400000, 
-                 "3oaK": 300000, 
-                 "2P": 200000, 
-                 "1P": 100000, 
-                 "HC": 0}
+        TYPES = {"5oaK": 7,
+                 "4oaK": 6, 
+                 "FH": 5, 
+                 "3oaK": 4, 
+                 "2P": 3, 
+                 "1P": 2, 
+                 "HC": 1}
         
         # Put the hand in a dict for easy counting
         a = {}
@@ -98,7 +103,7 @@ class Hand:
         # Check for 5 of a kind
         if 5 in a.values():
             self.type = "5oaK"
-            self.score += TYPES[self.type]
+            self.primary_score = TYPES[self.type]
             return TYPES[self.type]
         
         # Check for 4 of a kind
@@ -110,31 +115,31 @@ class Hand:
         # check for Full House
         if len(set([card.label for card in self.cards])) == 2:
             self.type = "FH"
-            self.score += TYPES[self.type]
+            self.primary_score = TYPES[self.type]
             return TYPES[self.type]
         
         # Check for 3 of a kind
         if 3 in a.values():
             self.type = "3oaK"
-            self.score += TYPES[self.type]
+            self.primary_score = TYPES[self.type]
             return TYPES[self.type]
         
         # Check for 2 pairs
         if len(a) == 3:
             self.type = "2P"
-            self.score += TYPES[self.type]
+            self.primary_score = TYPES[self.type]
             return TYPES[self.type]
         
         # Check for 1 pair
         if len(a) == 4:
             self.type = "1P"
-            self.score += TYPES[self.type]
+            self.primary_score = TYPES[self.type]
             return TYPES[self.type]
         
         # High Card
         if not self.type:
             self.type = "HC"
-            self.score += TYPES[self.type]
+            self.primary_score = TYPES[self.type]
             return TYPES[self.type]
 
 
@@ -170,15 +175,18 @@ def test_sanity():
 def test_score():
     """ Test scoring """
     logger.info("")
-    assert Hand(cards=[Card(label="A"), Card(label="A"), Card(label="A"), Card(label="A"), Card(label="A")], bid=1).score == 613706
     assert (Hand(cards=[Card(label="A"), Card(label="Q"), Card(label="A"), Card(label="Q"), Card(label="A")], bid=1).score <
             Hand(cards=[Card(label="2"), Card(label="2"), Card(label="2"), Card(label="2"), Card(label="7")], bid=1).score)
     
 def test_hand():
     """ Test Hand """
-    cards_labels = "3344T"
-    hand = Hand(cards=[Card(label=label) for label in cards_labels], bid=1)
-    logger.info(hand)
+    h1cards = "46646"
+    h2cards = "38838"
+    hand1 = Hand(cards=[Card(label=label) for label in h1cards], bid=1)
+    hand2 = Hand(cards=[Card(label=label) for label in h2cards], bid=1)
+    logger.info(f"Assert {hand1} > {hand2}")
+    assert hand1 > hand2
+    assert hand1.score > hand2.score
 
 def test_sample_1():
     """ Test Sample 1"""
@@ -197,6 +205,7 @@ def test_sample_1():
         except AssertionError as e:
             logger.error(e)
     logger.info(f"Winnings: {winnings}")
+    assert winnings == 6440
 
 
 
@@ -204,11 +213,9 @@ def test_part_1():
     """Test part 1"""
     hands = Hands(WORKING_DIR + 'input.txt')
     logger.info("")
-    #for hand in hands.hands:
-        #logger.info(f"Hand: {hand}")
-
     winnings = 0
     sorted_hands = sorted(hands.hands, key=lambda x: x.score, reverse=False)
+    #sorted_hands = sorted(hands.hands, reverse=False)
     for rank, hand in enumerate(sorted_hands):
         winnings += hand.bid * (rank + 1)
         logger.info(f"Sorted Hand: {hand}, Rank: {rank + 1}, Total Winnings: {winnings:,}")
@@ -219,7 +226,6 @@ def test_part_1():
         except AssertionError as e:
             logger.error(e)
     logger.info(f"Winnings: {winnings}")
-    #assert winnings != 253983146
 
 def test_sample_2():
     """Test part 2"""
