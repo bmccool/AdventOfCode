62903a6b0b1c91d9eccd02a383c3544cb7e39b7a
Brendon McCool <brendonmccool@gmail.com>
2023-12-07 18:43:25 -0500

    All tests passing

--- 2023/Day07/test_07.py
+++ 2023/Day07/test_07.py
@@ -12,17 +12,22 @@ logger = Logger(LoggerKwargs(
 WORKING_DIR = '2023/Day07/'
 
 
-CARDS = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
+CARDS_NORMAL = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
+CARDS_JOKER = ["J", "2", "3", "4", "5", "6", "7", "8", "9", "T", "Q", "K", "A"]
 
 @dataclass
 class Card:
     """ A card has a label and a value """
     label: str
     value: int = 0
+    joker_rules: bool = False
 
     def __post_init__(self) -> None:
         self.label = self.label.upper()
-        self.value = CARDS.index(self.label) + 2
+        if self.joker_rules:
+            self.value = CARDS_JOKER.index(self.label) + 2
+        else:
+            self.value = CARDS_NORMAL.index(self.label) + 2
 
     def __str__(self) -> str:
         return f"{self.label}"
@@ -36,13 +41,14 @@ class Hand:
     type: str = ""
     primary_score = 0
     secondary_score = 0
+    joker_rules: bool = False
 
     def __str__(self) -> str:
         return f"{"".join([card.label for card in self.cards])}, ({self.type}: {self.score:_}), Bid: {self.bid}"
-    
+
     def __repr__(self) -> str:
         return f"{self.__str__()}"
-    
+
     def __post_init__(self) -> None:
         self.primary_score = self.get_primary_score_number()
         self.secondary_score = self.get_secondary_score_number()
@@ -71,14 +77,11 @@ class Hand:
 
     def get_secondary_score_number(self) -> int:
         """ Get the tiebreaker score"""
-        # Must be < 1000, the resolution of the primary score
-        # Score each card and give weight to first cards
         secondary_score = int("0x" + "".join([hex(card.value).strip("0x") for card in self.cards]), 16)
         logger.debug(f"Secondary Score: {secondary_score}")
         self.secondary_score = secondary_score
         return self.secondary_score
-    
-    
+
     def get_primary_score_number(self) -> int:
         """ Score the hand """
         TYPES = {"5oaK": 7,
@@ -88,7 +91,7 @@ class Hand:
                  "2P": 3, 
                  "1P": 2, 
                  "HC": 1}
-        
+
         # Put the hand in a dict for easy counting
         a = {}
         for card in self.cards:
@@ -96,17 +99,18 @@ class Hand:
                 a[card.label] = 0
             a[card.label] += 1
 
-        # Are there any jokers?
-        jokers = len([card for card in self.cards if card.label == "J"])
-        #logger.info(f"{jokers} Jokers in {self}")
-
+        # Are there any jokers? # Do they count?
+        if self.joker_rules:
+            jokers = len([card for card in self.cards if card.label == "J"])
+        else:
+            jokers = 0
 
         # Check for 5 of a kind
         if 5 in a.values():
             self.type = "5oaK"
             self.primary_score = TYPES[self.type]
             return TYPES[self.type]
-        
+
         # Check for 4 of a kind
         if 4 in a.values():
             self.type = "4oaK"
@@ -114,7 +118,7 @@ class Hand:
                 self.type = "5oaK"
             self.score += TYPES[self.type]
             return TYPES[self.type]
-        
+
         # check for Full House
         if len(set([card.label for card in self.cards])) == 2:
             self.type = "FH"
@@ -122,7 +126,7 @@ class Hand:
                 self.type = "5oaK"
             self.primary_score = TYPES[self.type]
             return TYPES[self.type]
-        
+
         # Check for 3 of a kind
         if 3 in a.values():
             self.type = "3oaK"
@@ -132,7 +136,7 @@ class Hand:
                 self.type = "4oaK"
             self.primary_score = TYPES[self.type]
             return TYPES[self.type]
-        
+
         # Check for 2 pairs
         if len(a) == 3:
             self.type = "2P"
@@ -142,7 +146,7 @@ class Hand:
                 self.type = "FH"
             self.primary_score = TYPES[self.type]
             return TYPES[self.type]
-        
+
         # Check for 1 pair
         if len(a) == 4:
             self.type = "1P"
@@ -152,7 +156,7 @@ class Hand:
                 self.type = "3oaK"
             self.primary_score = TYPES[self.type]
             return TYPES[self.type]
-        
+
         # High Card
         if not self.type:
             self.type = "HC"
@@ -161,14 +165,10 @@ class Hand:
             self.primary_score = TYPES[self.type]
             return TYPES[self.type]
 
-
-
-
-
-
 class Hands:
     """ Class for parsing Camel Cards Hands """
-    def __init__(self, filename: str):
+    def __init__(self, filename: str, joker_rules: bool = False):
+        self.joker_rules = joker_rules
         self.filename = filename
         self.hands: List[Hand] = []
         self.parse()
@@ -179,11 +179,11 @@ class Hands:
             for line in file:
                 line = line.strip()
                 parts = line.split()
-                self.hands.append(Hand(cards=[Card(label=label) for label in parts[0]], bid=int(parts[-1])))
+                self.hands.append(Hand(cards=[Card(label=label, joker_rules=self.joker_rules) for label in parts[0]], bid=int(parts[-1]), joker_rules=self.joker_rules))
 
     def __repr__(self) -> str:
         return f"{self.hands}"
-    
+
     def __str__(self) -> str:
         return f"{self.hands}"
 
@@ -196,34 +196,34 @@ def test_score():
     logger.info("")
     assert (Hand(cards=[Card(label="A"), Card(label="Q"), Card(label="A"), Card(label="Q"), Card(label="A")], bid=1).score <
             Hand(cards=[Card(label="2"), Card(label="2"), Card(label="2"), Card(label="2"), Card(label="7")], bid=1).score)
-    
-def do_hand_test(card_str: str, expected_type: str):
+
+def do_joker_test(card_str: str, expected_type: str):
     """ Test a hand """
-    hand = Hand(cards=[Card(label=label) for label in card_str], bid=1)
+    hand = Hand(cards=[Card(label=label, joker_rules=True) for label in card_str], bid=1, joker_rules=True)
     logger.info(f"Expect Hand: {hand} is {expected_type}")
     assert hand.type == expected_type
-    
+
 def test_joker_types():
     """ Test hand types with joker rules """
     logger.info("")
-    do_hand_test("23456", "HC")
-    do_hand_test("J3456", "1P")
-    do_hand_test("JJ456", "3oaK")
-    do_hand_test("JJJ56", "4oaK")
-    do_hand_test("JJJJ6", "5oaK")
-    do_hand_test("JJJJJ", "5oaK")
-    do_hand_test("23455", "1P")
-    do_hand_test("J3455", "3oaK")
-    do_hand_test("JJ455", "4oaK")
-    do_hand_test("JJJ55", "5oaK")
-    do_hand_test("23344", "2P")
-    do_hand_test("J3344", "FH")
-    do_hand_test("2JJ44", "4oaK")
-    do_hand_test("42333", "3oaK")
-    do_hand_test("J2333", "4oaK")
-    do_hand_test("JJ333", "5oaK")
-    do_hand_test("42JJJ", "4oaK")
-    
+    do_joker_test("23456", "HC")
+    do_joker_test("J3456", "1P")
+    do_joker_test("JJ456", "3oaK")
+    do_joker_test("JJJ56", "4oaK")
+    do_joker_test("JJJJ6", "5oaK")
+    do_joker_test("JJJJJ", "5oaK")
+    do_joker_test("23455", "1P")
+    do_joker_test("J3455", "3oaK")
+    do_joker_test("JJ455", "4oaK")
+    do_joker_test("JJJ55", "5oaK")
+    do_joker_test("23344", "2P")
+    do_joker_test("J3344", "FH")
+    do_joker_test("2JJ44", "4oaK")
+    do_joker_test("42333", "3oaK")
+    do_joker_test("J2333", "4oaK")
+    do_joker_test("JJ333", "5oaK")
+    do_joker_test("42JJJ", "4oaK")
+
 def test_hand():
     """ Test Hand """
     h1cards = "46646"
@@ -234,7 +234,6 @@ def test_hand():
     assert hand1 > hand2
     assert hand1.score > hand2.score
 
-@pytest.mark.skip("Uses wrong rules")
 def test_sample_1():
     """ Test Sample 1"""
     logger.info("")
@@ -254,8 +253,6 @@ def test_sample_1():
     logger.info(f"Winnings: {winnings}")
     assert winnings == 6440
 
-
-
 def test_part_1():
     """Test part 1"""
     logger.info("")
@@ -273,11 +270,12 @@ def test_part_1():
         except AssertionError as e:
             logger.error(e)
     logger.info(f"Winnings: {winnings}")
+    assert winnings == 254024898
 
 def test_sample_2():
     """Test part 2"""
     logger.info("")
-    hands = Hands(WORKING_DIR + 'input_sample.txt')
+    hands = Hands(WORKING_DIR + 'input_sample.txt', joker_rules=True)
     winnings = 0
     sorted_hands = sorted(hands.hands, key=lambda x: x.score, reverse=False)
     for rank, hand in enumerate(sorted_hands):
@@ -291,12 +289,11 @@ def test_sample_2():
             logger.error(e)
     logger.info(f"Winnings: {winnings}")
     assert winnings == 5905
-    
 
 def test_part_2():
     """Test part 2"""
     logger.info("")
-    hands = Hands(WORKING_DIR + 'input.txt')
+    hands = Hands(WORKING_DIR + 'input.txt', joker_rules=True)
     winnings = 0
     sorted_hands = sorted(hands.hands, key=lambda x: x.score, reverse=False)
     for rank, hand in enumerate(sorted_hands):
@@ -311,4 +308,4 @@ def test_part_2():
         except AssertionError as e:
             logger.error(e)
     logger.info(f"Winnings: {winnings}")
-    
+    assert winnings == 254115617
